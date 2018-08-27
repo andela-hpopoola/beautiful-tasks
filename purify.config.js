338b0e7b8be20b47da98f3960d46aae4af03a40a
Haruna Popoola <haruna.popoola@andela.com>
2018-08-27 22:53:22 +0100

    Add purify css to remove unused css

---
+++ purify.config.js
@@ -0,0 +1,16 @@
+const purify = require("purify-css");
+const content = ["**/src/**/*.jsx", "**/public/*.html"]; // the contents to look at to determine css
+const css = ["./src/App.css"]; // the css content you need to purify
+
+const options = {
+  // purified css
+  output: "./src/App.css",
+
+  // Will minify CSS code in addition to purify.
+  minify: false,
+
+  // Logs out removed selectors.
+  rejected: false
+};
+
+purify(content, css, options);
